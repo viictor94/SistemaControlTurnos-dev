ec7ff5a77ba85c3fc8bdc595c840efe45df7ec3a
viictor94 <76214934+viictor94@users.noreply.github.com>
2026-08-04 17:18:19 -0300

    Update js.js

--- admin/js.js
+++ admin/js.js
@@ -13,7 +13,13 @@ function cargarDashboard(){
 
     .then(r => r.json())
 
-    .then(cargarKPIs)
+   .then(function(datos){
+
+    cargarKPIs(datos);
+
+    cargarIncidencias(datos.listaIncidencias);
+
+})
 
     .catch(function(error){
 
@@ -27,14 +33,49 @@ function cargarKPIs(datos){
 
     document.getElementById("kpiPresentes").innerHTML =
         datos.presentes;
-
     document.getElementById("kpiAlmuerzo").innerHTML =
         datos.almuerzo;
-
     document.getElementById("kpiAusentes").innerHTML =
         datos.ausentes;
-
     document.getElementById("kpiIncidencias").innerHTML =
         datos.incidencias;
+}
 
+function cargarIncidencias(lista){
+    const contenedor =
+        document.getElementById("listaIncidencias");
+    contenedor.innerHTML = "";
+    if(lista.length == 0){
+        contenedor.innerHTML = `
+            <div class="sin-datos">
+                No se registraron incidencias hoy.
+            </div>
+        `;
+
+        return;
+    }
+    let sucursalActual = "";
+    lista.forEach(function(item){
+        if(item.sucursal != sucursalActual){
+            sucursalActual = item.sucursal;
+            contenedor.innerHTML += `
+                <div class="tituloSucursal">
+                    ${item.sucursal}
+                </div>
+            `;
+        }
+        contenedor.innerHTML += `
+            <div class="incidencia">
+                <div class="empleado">
+                    ${item.empleado}
+                </div>
+                <div class="tipo">
+                    ${item.tipo}
+                </div>
+                <div class="observacion">
+                    ${item.observacion}
+                </div>
+            </div>
+        `;
+    });
 }
