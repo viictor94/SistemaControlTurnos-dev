e6678e34a33a60ac28568b240519336e93521e4d
viictor94 <76214934+viictor94@users.noreply.github.com>
2026-08-04 16:55:38 -0300

    Update js.js

--- admin/js.js
+++ admin/js.js
@@ -1,1 +1,40 @@
+/*=========================================
+  DASHBOARD
+=========================================*/
 
+window.addEventListener("load", cargarDashboard);
+
+function cargarDashboard(){
+
+    fetch(
+        "TU_URL_DEL_APPS_SCRIPT"
+        + "?accion=dashboard"
+    )
+
+    .then(r => r.json())
+
+    .then(cargarKPIs)
+
+    .catch(function(error){
+
+        console.error(error);
+
+    });
+
+}
+
+function cargarKPIs(datos){
+
+    document.getElementById("kpiPresentes").innerHTML =
+        datos.presentes;
+
+    document.getElementById("kpiAlmuerzo").innerHTML =
+        datos.almuerzo;
+
+    document.getElementById("kpiAusentes").innerHTML =
+        datos.ausentes;
+
+    document.getElementById("kpiIncidencias").innerHTML =
+        datos.incidencias;
+
+}
